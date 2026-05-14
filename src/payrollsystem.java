8c62bc28a6c602f41742081de2335fe4bcb031a2
Aiswarya Tejas <aiswaryatejas@gmail.com>
2026-05-14 10:00:27 +0530

    payrollsystem

--- src/payrollsystem.java
+++ src/payrollsystem.java
@@ -1,41 +1,86 @@
 package Employeepayrollmanagement;
 class PayrollSystem {
+
     int payrollId;
     Employee[] employeeList;
+    int empCount;
 
-    PayrollSystem(int payrollId,int size) {
+    PayrollSystem(int payrollId, int size) {
+
         this.payrollId = payrollId;
         employeeList = new Employee[size];
+        empCount = 0;
     }
-    // Calculate salary
+
+    void addEmp(Employee emp) {
+
+        if (empCount < employeeList.length) {
+
+            employeeList[empCount] = emp;
+            empCount++;
+
+            System.out.println("\nEmployee Added Successfully");
+        } else {
+            System.out.println("Employee list is full!");
+        }
+    }
+
+    Employee getEmployeeById(int id) {
+
+        for (int i = 0; i < empCount; i++) {
+
+            if (employeeList[i].id == id) {
+                return employeeList[i];
+            }
+        }
+
+        return null;
+    }
+
     SalarySlip calculateSalary(Employee emp) {
-        
+
         if (!emp.attendanceApproved) {
-
             System.out.println("\nAttendance Not Approved");
-
             return null;
         }
 
         int totalWorkingDays = 30;
 
-        double dailySalary =emp.basicSalary / totalWorkingDays;
+        double dailySalary = emp.basicSalary / totalWorkingDays;
 
         int absentDays = totalWorkingDays - emp.daysPresent;
 
-        double absenceDeduction =absentDays * dailySalary;
-                  
-        double netSalary =emp.basicSalary - absenceDeduction;
+        double deduction = absentDays * dailySalary;
 
-       
-        SalarySlip slip = new SalarySlip(emp.id,emp.basicSalary,emp.daysPresent,absentDays,absenceDeduction, netSalary);
+        double netSalary = emp.basicSalary - deduction;
 
-        return slip;
+        return new SalarySlip(
+                emp.id,
+                emp.basicSalary,
+                emp.daysPresent,
+                absentDays,
+                deduction,
+                netSalary
+        );
     }
 
-    void generatePayslip(SalarySlip slip) {
+    void viewSalarySlip(Employee emp) {
+
+        SalarySlip slip = calculateSalary(emp);
+
         if (slip != null) {
             slip.generateReport();
         }
+    }
+
+    void displayAllEmployees() {
+
+        System.out.println("\n===== ALL EMPLOYEES =====");
+
+        for (int i = 0; i < empCount; i++) {
+            employeeList[i].displayEmployee();
+        }
+
+        System.out.println("=========================");
     }
 }
