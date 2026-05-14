4a4fd0b587c00087b2b48ef3b41118750f9716f3
Aiswarya Tejas <aiswaryatejas@gmail.com>
2026-05-14 10:03:20 +0530

    HR

--- src/HR.java
+++ src/HR.java
@@ -1,34 +1,22 @@
 package Employeepayrollmanagement;
 
 class HR extends Person {
+
     HR(int id, String name) {
         super(id, name);
     }
+
     @Override
     void displayRole() {
-
         System.out.println("Role : HR");
     }
-    // Add employee
-    void addEmp(Employee[] employeeList,Employee emp,int index) {
 
-        employeeList[index] = emp;
-
-        System.out.println("\nEmployee Added Successfully");
+    void addEmp(PayrollSystem payroll, Employee emp) {
+        payroll.addEmp(emp);
     }
 
-    // Update employee
-    void updateEmp(Employee emp,String designation,double salary) {
-
-        emp.updateDetails(designation, salary);
-          System.out.println("\nEmployee Updated Successfully");
-    }
-
-    // Mark attendance
     void markAttendance(Employee emp, int days) {
-
         emp.daysPresent = days;
-
         System.out.println("\nAttendance Marked Successfully");
     }
 }
